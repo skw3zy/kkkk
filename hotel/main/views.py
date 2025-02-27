bb9393bce86fb02ab1201d770da846196dfecb2a
skw3zy <skw3zy@gmail.com>
2025-02-27 22:06:09 +0300

    модели
    создание модели для приема данных

--- hotel/main/views.py
+++ hotel/main/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import MyModelForm 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+def my_view(request):
+    if request.method == 'POST':
+        form = MyModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('succes')
+            else:
+                form = MyModelForm()
+                return render(request, 'myapp/my_form.html', {'form': form})
